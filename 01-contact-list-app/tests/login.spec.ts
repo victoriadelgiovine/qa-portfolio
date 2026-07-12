a2c9f670b4d9c819c1c995663123c90839860f1b
victoriadelgiovine <victoriadelgiovine@gmail.com>
2026-07-12 13:44:16 +0200

    Update login.spec.ts

--- 01-contact-list-app/tests/login.spec.ts
+++ 01-contact-list-app/tests/login.spec.ts
@@ -1,1 +1,22 @@
-login.spec.ts
+import { test, expect } from '@playwright/test';
+
+const URL = 'https://thinking-tester-contact-list.herokuapp.com';
+
+test('Login exitoso con credenciales válidas', async ({ page }) => {
+  await page.goto(URL);
+  
+  await page.fill('input[placeholder="Email"]', 'nuevotester@tester.com');
+    await page.fill('input[placeholder="Password"]', '123456789');
+  await page.click('button[id="submit"]');
+  
+  await expect(page).toHaveURL(`${URL}/contactList`);
+});
+test('Login fallido con credenciales inválidas', async ({ page }) => {
+  await page.goto(URL);
+  
+  await page.fill('input[placeholder="Email"]', 'usuariofalso@test.com');
+  await page.fill('input[placeholder="Password"]', 'contraseñamala');
+  await page.click('button[id="submit"]');
+  
+  await expect(page.locator('#error')).toBeVisible();
+});
